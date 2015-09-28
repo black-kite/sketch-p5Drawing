9743d4eb7170e159cddda3c471aeed8f845e2843
Milan Gress MBA <dev@milangress.de>
2015-09-28 21:52:33 +0200

    save button

--- sketch.js
+++ sketch.js
@@ -1,8 +1,12 @@
 var smallPoint = 20;
 var largePoint = 50;
-
 var hueColor = 0;
 var spread = 2.7;
+
+
+
+var vec;
+var saveing = false;
 
 function setup() {
   var canvas = createCanvas(windowWidth, windowHeight);
@@ -10,7 +14,10 @@ function setup() {
 
   spreadSlider = createSlider(0.2, 10, 2.7);
   spreadSlider.position(20, 20);
-  //imageMode(CENTER);
+  button = createButton('save');
+  button.position(150, 65);
+  button.mousePressed(savePng);
+  
   colorMode(HSB, 100, 100, 100, 1);
   fill(100, 100, 100, 0.005)
   noStroke();
@@ -20,22 +27,26 @@ function draw() {
 
   if (mouseIsPressed) {
     for (var i = 0; i < 20; i++) {
-      var vec = p5.Vector.random2D();
+      vec = p5.Vector.random2D();
       vec.mult(random(1, 20 * spreadSlider.value()));
       var pointsize = random(smallPoint, largePoint);
       var x = mouseX + vec.x;
       var y = mouseY + vec.y;
 
       ellipse(x, y, pointsize, pointsize);
-      hueColor = map(second() % 30, 0, 60, 0, 100);
-      fill(hueColor, 100, 100, 0.01);
+      hueColor = map(second(), 0, 60, 0, 100);
+      fill(hueColor, 50, 100, 0.01);
     }
   }
-  
+
 }
+  
 
 function keyPressed() {
   if (key === " ") {
     clear();
   }
+}
+function savePng() {
+  saveCanvas(canvas, 'myDrawing', 'jpg');
 }

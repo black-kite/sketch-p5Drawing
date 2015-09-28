74612b0141cb2ba229917e5236831d3f8afe4934
Milan Gress MBA <dev@milangress.de>
2015-09-28 21:18:38 +0200

    first draw

---
+++ sketch.js
@@ -0,0 +1,39 @@
+var smallPoint = 20;
+var largePoint = 50;
+
+var hueColor = 0;
+var spread = 2.7;
+
+function setup() {
+  var canvas = createCanvas(windowWidth, windowHeight);
+  canvas.id('container');
+
+  spreadSlider = createSlider(0.2, 10, 2.7);
+  spreadSlider.position(20, 20);
+
+  //imageMode(CENTER);
+  colorMode(HSB, 100, 100, 100, 1);
+  fill(100, 100, 100, 0.005)
+  noStroke();
+  angleMode(RADIANS);
+}
+
+function draw() {
+
+  if (mouseIsPressed) {
+
+    for (var i = 0; i < 20; i++) {
+
+      var vec = p5.Vector.random2D();
+      vec.mult(random(1, 20 * spreadSlider.value()));
+      var pointsize = random(smallPoint, largePoint);
+      var x = mouseX + vec.x;
+      var y = mouseY + vec.y;
+
+      ellipse(x, y, pointsize, pointsize);
+      hueColor = map(second(), 0, 60, 0, 100);
+      fill(hueColor, 100, 100, 0.01);
+    }
+
+  }
+}
